d5eeb6f19b154df1fb29f53c832eeab483293b06
OGGY46 <109297137+OGGY46@users.noreply.github.com>
2022-07-14 21:43:07 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -28,14 +28,14 @@ async def start(bot, update):
             await update.reply_cached_media(
                 file_id,
                 quote=True,
-                caption = caption,
+                caption = f"{file_name}  /n @malrock",
                 parse_mode="html",
                 reply_markup=InlineKeyboardMarkup(
                     [
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🥰OUR GROUP🥰', url="https://t.me/MOVIES WORLD⭕️"
                                 )
                         ]
                     ]
@@ -47,7 +47,8 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
+        InlineKeyboardButton('🥰SHARE GROUP😘🥰', url='https://t.me/MOVIES WORLD⭕️"
+                                )'),
         InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
     ],[
         InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
